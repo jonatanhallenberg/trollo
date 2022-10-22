2d60372864253ce6f3f612e261ac749d217133ac
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-10-22 12:12:23 +0200

    Add react app

---
+++ packages/trollo-spa/src/app/app.tsx
@@ -0,0 +1,34 @@
+
+import styled from 'styled-components';
+import NxWelcome from "./nx-welcome";
+
+import { Route, Routes, Link } from 'react-router-dom';
+
+const StyledApp = styled.div`
+    // Your style here
+`;
+
+
+export const App = () => {
+
+  return (
+    <StyledApp>
+      <Routes>
+        <Route  
+          path="/"
+          element={
+            <div>This is the generated root route. <Link to="/page-2">Click here for page 2.</Link></div>
+          }
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div><Link to="/">Click here to go back to root page.</Link></div>
+          }
+        />
+      </Routes>
+    </StyledApp>
+  );
+}
+
+export default App;
